00aa70deeac7801c686eb4573d72894b8de44a2c
lunarjuun <157792207+lunarjuun@users.noreply.github.com>
2025-04-03 11:04:50 +0200

    kom opp i trøbbel.... skal fikse dealerSum

--- code/blackjack.js
+++ code/blackjack.js
@@ -41,8 +41,27 @@ function shuffleDeck() {
 function startGame() {
     hidden = deck.pop() //fjerner et kort fra slutten av array
     dealerSum += getValue(hidden);
+    dealerAceCount += checkAce(hidden);
+    console.log(hidden);
+    console.log(dealerSum);
 }
 
 function getValue(card) {
-    
+    let data = card.split(); // 4(value) C(type). så "split" på "-" betyr at koden deler opp i to så vi får en array som ["4", "C"]
+    let value = data[0];
+
+    if (isNaN(value)) { // J Q K har samme Value -- sjekker om Value har nummer, hvis ikke er det A (ess)
+        if (value == "A") {
+            return 11;
+        }
+        return 10;
+    }
+    return parseInt(value);
+}
+
+function checkAce(card) {
+    if (card[0] == "A") {  //card er en String, hvis første nummer er "A", return 1
+        return 1;
+    }
+    return 0;
 }
